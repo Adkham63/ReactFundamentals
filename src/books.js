195a8461b1e7782303ddf7fef6abdf8295ef0ef4
Adkham63 <karimovadham63@gmail.com>
2024-02-09 20:08:33 +0500

    first commit

---
+++ src/books.js
@@ -0,0 +1,24 @@
+export const books = [
+{
+    title: 'Interesting Facts For Curious Minds',
+    author: 'Jordan Moore',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg',
+    id: 1,
+ },
+  
+ {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+    id: 2,
+  },
+
+  {
+    author: 'Stephen King',
+    title: 'Fairy Tale',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81blQfKsLtL._AC_UL232_SR232,232_.jpg',
+    id: 3,
+  },
+];
+  
+  
